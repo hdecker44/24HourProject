5ef5db090dadb0432b263b085cf781e767fbc0b4
Henry Decker <henryjdecker@gmail.com>
2020-03-02 17:32:37 -0500

    Add project files.

---
+++ ClassLibrary1/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Post
+    {
+        [Key]
+        public string PostId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public Guid? UserId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
